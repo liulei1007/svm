a6f2944c7445b0b63567304052002efdda18c033
liulei1007 <you@example.com>
2016-04-26 09:25:04 +0800

    liulei

--- debug/js/login.js
+++ debug/js/login.js
@@ -2,5 +2,16 @@ $(function(){
 	plumeLog("进入login模板自定义js-"+plumeTime());
 	$(".login-btn").bind("click",function(){
 		window.location.href="index.html";
-	})
+	});
+	$(".reg-btn").bind("click",function(){
+		window.location.href="index.html?regsuccess";
+	});
+	$(".login-head-btn .btn1").bind("click",function(){
+		$(".reg-block").fadeOut();
+		$(".login-block").fadeIn();
+	});
+	$(".login-head-btn .btn2").bind("click",function(){
+		$(".login-block").fadeOut();
+		$(".reg-block").fadeIn();
+	});
 });
