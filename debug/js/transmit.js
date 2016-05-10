5b40626b8e619f21b346892cf01569a32e2a6ca3
liulei1007 <you@example.com>
2016-05-10 19:54:44 +0800

    liulei

--- debug/js/transmit.js
+++ debug/js/transmit.js
@@ -1,57 +1,25 @@
+var transmit_a = 0;
+var transmit_d = true;
+var transmit_loop;
 $(function () {
     plumeLog("进入transmit模板自定义js-" + plumeTime());
-    var temp = '';
-    for (var i = 1; i < 36; i++) {
-        temp += '<div class="popcenter load1 load1img' + i + '"><img src="images/loading/' + i + '.png"></div>'
-    }
-    for (var i = 1; i < 43; i++) {
-        temp += '<div class="popcenter load2 load2img' + i + '"><img src="images/loading_2/' + i + '.png"></div>'
-    }
-    $(document.body).append(temp);
-    var path = window.location.href + "";
-    var p = path.substring(path.indexOf("?") + 1);
-    if (p == 1) {
-        showLoad();
-    } else if (p == 2) {
-        showLoad1();
-    } else {
-        showLoad2();
-    }
+    clearTimeout(transmit_loop)
+    transmit_showLoad();
 })
-var a = 1;
-var d = true;
-function showLoad() {
-    $(".load1").hide();
-    $(".load1img" + a).show();
-    if (d) {
-        a++;
-    } else {
-        a--
-    }
-    if (a == 35) {
-        d = false;
-    }
-    if (a == 1) {
-        d = true;
-    }
-    setTimeout("showLoad()", 35);
-}
 
-function showLoad1() {
-    $(".load1").hide();
-    $(".load1img" + a).show();
-    a++;
-    if (a == 35) {
-        a = 1;
+function transmit_showLoad() {
+    $(".loading").hide();
+    $($(".loading")[transmit_a]).show();
+    if (transmit_d) {
+        transmit_a++;
+    } else {
+        transmit_a--
     }
-    setTimeout("showLoad1()", 50);
-}
-function showLoad2() {
-    $(".load2").hide();
-    $(".load2img" + a).show();
-    a++;
-    if (a == 42) {
-        a = 1;
+    if (transmit_a == 34) {
+        transmit_d = false;
     }
-    setTimeout("showLoad2()", 80);
+    if (transmit_a == 0) {
+        transmit_d = true;
+    }
+    transmit_loop=setTimeout("transmit_showLoad()", 35);
 }
