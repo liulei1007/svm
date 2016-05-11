c99d6228bb593c6827a37f3deb27e0cc8a6404e9
Liu <450683797@qq.com>
2016-05-11 19:17:08 +0800

    new index

--- debug/js/welcome.js
+++ debug/js/welcome.js
@@ -1,4 +1,14 @@
 $(function(){
-	plumeLog("进入welcome模板自定义js-"+plumeTime());
 	
+	// plumeLog("进入welcome模板自定义js-"+plumeTime());
+	$(".bw-item-title").on("click", function() {
+		var $block = $(this).parents(".bw-block-item");
+		$block.toggleClass("contractive");
+		if ($block.hasClass("contractive")) {
+			$(this).siblings(".bw-item-table, .bw-item-btns").slideUp();
+		}
+		else {
+			$(this).siblings(".bw-item-table, .bw-item-btns").slideDown();
+		}
+	});
 })
