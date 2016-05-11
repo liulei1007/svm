05a2f21e7ac40b53ae2d27df69721ae758adc4fd
liulei1007 <you@example.com>
2016-05-11 20:17:41 +0800

    liulei

--- debug/js/login.js
+++ debug/js/login.js
@@ -1,25 +1,90 @@
-$(function(){
-	plumeLog("进入login模板自定义js-"+plumeTime());
-	$(".login-btn-brand").bind("click",function(){
-		sessionStorage.auth=[1,2,4,5,6,8];
-		window.location.href="../index";
-	});
-	$(".login-btn-supplier").bind("click",function(){
-		sessionStorage.auth=[3,4,5,7];
-		window.location.href="../index";
-	});
-	$(".reg-btn").bind("click",function(){
-		window.location.href="../secondreg?fullscreen";
-	});
-	$(".login-head-btn .btn1").bind("click",function(){
-		$(".reg-block").fadeOut();
-		$(".login-block").fadeIn();
-	});
-	$(".login-head-btn .btn2").bind("click",function(){
-		$(".login-block").fadeOut();
-		$(".reg-block").fadeIn();
-	});
-	$(".fetchpwd").bind("click",function(){
-		window.location.href="../changepwd?fullscreen";
-	});
+$(function () {
+    plumeLog("进入login模板自定义js-" + plumeTime());
+    $(".login-btn-brand").bind("click", function () {
+        sessionStorage.auth = [1, 2, 4, 5, 6, 8];
+        window.location.href = "../index";
+    });
+    $(".login-btn-supplier").bind("click", function () {
+        sessionStorage.auth = [3, 4, 5, 7];
+        window.location.href = "../index";
+    });
+
+    $(".login-head-btn .btn1").bind("click", function () {
+        $(".reg-block").fadeOut();
+        $(".login-block").fadeIn();
+    });
+    $(".login-head-btn .btn2").bind("click", function () {
+        $(".login-block").fadeOut();
+        $(".reg-block").fadeIn();
+    });
+    $(".fetchpwd").bind("click", function () {
+        window.location.href = "../changepwd?fullscreen";
+    });
+    $("#send").bind("click", function () {
+        $(".form-block-msg").hide();
+        var tel = $("#tel").val();
+        if (isMobile(tel)) {
+            $.get(plumeApi["sendMsg"] + "/" + tel + "/10003", {}, function (data) {
+                if (data.ok) {
+                    $(".reg-msg2").text("短信验证码发送成功").fadeIn();
+                }
+            });
+        } else {
+            $(".reg-msg1").text("手机号输入错误").fadeIn();
+        }
+    });
+    $("#send").bind("click", function () {
+        $(".form-block-msg").hide();
+        var tel = $("#tel").val();
+        if (isMobile(tel)) {
+            $.get(plumeApi["sendMsg"] + "/" + tel + "/10003", {}, function (data) {
+                if (data.ok) {
+                    $(".reg-msg2").text("短信验证码发送成功").fadeIn();
+                }
+            });
+        } else {
+            $(".reg-msg1").text("手机号输入错误").fadeIn();
+        }
+    });
+    $(".reg-btn").bind("click", function () {
+        $(".form-block-msg").hide();
+        var tel = $("#tel").val();
+        var pwd = $("#pwd").val();
+        var msgcode = $("#msgcode").val();
+        var pram_str = '{';
+        pram_str += '"mobilePhone": "' + tel + '",';
+        pram_str += '"password": "' + pwd + '",';
+        pram_str += '"rePassword": "' + pwd + '",';
+        pram_str += '"regVerifycode": "' + msgcode + '"';
+        pram_str += '}';
+        if (!isMobile(tel)) {
+            $(".reg-msg1").text("手机号输入错误").fadeIn();
+            return;
+        }
+        if (pwd == "") {
+            $(".reg-msg1").text("请输入密码").fadeIn();
+            return;
+        }
+        if (msgcode == "") {
+            $(".reg-msg1").text("请输入验证码").fadeIn();
+            return;
+        }
+        $.ajax({
+            type: "POST",
+            url: plumeApi["registerUserInfo"],
+            data: pram_str,
+            contentType: "application/json",
+            dataType: "json",
+            success: function (data) {
+                if (data.ok) {
+                    $(".reg-msg2").text("注册成功").fadeIn();
+                } else {
+                    $(".reg-msg1").text(data.resDescription).fadeIn();
+                }
+            }
+        });
+    });
+    function isMobile(n) {
+        return /^1\d{10}$/.test(n) && n != 11111111111;
+    }
 });
