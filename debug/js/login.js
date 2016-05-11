e8a40d6dcd327efdd4a7ef52dc0207fba6cb83c3
liulei1007 <you@example.com>
2016-05-11 20:34:35 +0800

    liulei

--- debug/js/login.js
+++ debug/js/login.js
@@ -33,19 +33,39 @@ $(function () {
             $(".reg-msg1").text("手机号输入错误").fadeIn();
         }
     });
-    $("#send").bind("click", function () {
+
+    $(".login-btn").bind("click", function () {
         $(".form-block-msg").hide();
-        var tel = $("#tel").val();
-        if (isMobile(tel)) {
-            $.get(plumeApi["sendMsg"] + "/" + tel + "/10003", {}, function (data) {
-                if (data.ok) {
-                    $(".reg-msg2").text("短信验证码发送成功").fadeIn();
-                }
-            });
-        } else {
-            $(".reg-msg1").text("手机号输入错误").fadeIn();
+        var logintel = $("#logintel").val();
+        var loginpwd = $("#loginpwd").val();
+        var pram_str = '{';
+        pram_str += '"mobilePhone": "",';
+        pram_str += '"password": ""';
+        pram_str += '}';
+        if (logintel == "") {
+            $(".login-msg1").text("请输入用户名").fadeIn();
+            return;
         }
+        if (loginpwd == "") {
+            $(".login-msg1").text("请输入密码").fadeIn();
+            return;
+        }
+        $.ajax({
+            type: "POST",
+            url: plumeApi["login"],
+            data: pram_str,
+            contentType: "application/json",
+            dataType: "json",
+            success: function (data) {
+                if (data.ok) {
+                    $(".login-msg2").text("登录成功,用户id:"+data.data+"仅验证登录,跳转请使用另外两个测试按钮.").fadeIn();
+                } else {
+                    $(".login-msg1").text(data.resDescription).fadeIn();
+                }
+            }
+        });
     });
+
     $(".reg-btn").bind("click", function () {
         $(".form-block-msg").hide();
         var tel = $("#tel").val();
