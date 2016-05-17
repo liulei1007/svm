1a13f44979a03229edee296a9a5c2b032544ccd9
Liu <450683797@qq.com>
2016-05-17 10:06:12 +0800

    new shop

--- debug/js/shopShow-agency.js
+++ debug/js/shopShow-agency.js
@@ -5,8 +5,14 @@ $(function() {
 	// 获取数据
 	getData();
 
+	// 绑定“返回”按钮
+	$(".body-typein .btn-back").bind("click", function() {
+		derict(this, "shopListAgency", "nochangeurl");
+	});
+
 	// 从服务器获取数据
 	function getData() {
+		loading();
 		$.ajax({
 			// url: "datas/shopDetail.txt",
 			url: "http://192.168.222.162:8080/shopInfo/getShopDetail/" + session.shopID,
@@ -14,6 +20,7 @@ $(function() {
 			dataType: "json",
 			contentType: "application/json; charset=utf-8",
 			success: function(result) {
+				unloading();
 				$(".body-typein").setPageData(result.data);
 			},
 			error:function(er){}
