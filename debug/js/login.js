14d710424d6766c35976bf8050b089d344d04cb1
zcl <294386108@qq.com>
2016-05-20 15:00:58 +0800

    登录之后session放入cookie中

--- debug/js/login.js
+++ debug/js/login.js
@@ -93,11 +93,16 @@ $(function () {
                 unloading();
                 if (data.ok) {
                     $(".login-msg2").text("登录成功,用户id:" + data.data + "仅验证登录,跳转请使用另外两个测试按钮.").fadeIn();
+                    $.cookie('JSESSIONID', data.data, {path: '/', domain: 'hxmklmall.cn'});
                     window.location.href = "index";
 
                 } else {
                     $(".login-msg1").text(data.resDescription).fadeIn();
                 }
+            },
+            error:function() {
+                unloading();
+                $(".login-msg1").text("网路异常").fadeIn();
             }
         });
     });
