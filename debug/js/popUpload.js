a494e837b3f3e9554eae6b4bb751e912c324aaa4
liulei1007 <you@example.com>
2016-05-20 15:36:39 +0800

    liulei

--- debug/js/popUpload.js
+++ debug/js/popUpload.js
@@ -9,6 +9,19 @@ $(function () {
         unloading();
         if(data.ok){
             $("#filepath").val(data.data);
+            var temp='<li class="goodsPic">';
+            temp+='<img class="cmg-goodsimgs" src="'+"http://10.11.25.215"+$("#filepath").val()+'">';
+            temp+='<div class="upload-btn upload-btn-left">';
+            temp+='<div class="arrow-left"></div>';
+            temp+='</div>';
+            temp+='<div class="upload-btn upload-btn-right">';
+            temp+='<div class="arrow-right"></div>';
+            temp+='</div>';
+            temp+='<div class="upload-btn upload-btn-delect">';
+            temp+='<div class="arrow-close"></div>';
+            temp+='</div>';
+            temp+='</li>';
+            $(".goodsPic-upload").append(temp);
         }else{
             alert(data.resDescription);
         }
