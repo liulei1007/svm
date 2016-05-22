13a374c51e15a81ad17f487206425826da0de426
alex <alexguoliang@163.com>
2016-05-22 17:26:36 +0800

    12313

--- debug/js/goodsDataManage.js
+++ debug/js/goodsDataManage.js
@@ -52,11 +52,11 @@ $(function () {
     //获取表格数据
     function getTableData() {
         var newData = JSON.stringify(datas)
-        $("[list-node]").remove();
+       
         loading();
         $.ajax({
             type: "POST",
-            url: plumeApi["listProductInfo"],
+            url: plumeApi["listProductInfo"]+"?currentPage=1&onePageCount=10",
             data: newData,
             contentType: "application/json",
             dataType: "json",
@@ -64,52 +64,26 @@ $(function () {
                 unloading();
                 totalPage = Math.ceil(data.countRecord / 10);
                 newPage(totalPage, function (i) {
-
-                })
+                    loading();
+                    $.ajax({
+                        type: "POST",
+                        url: plumeApi["listProductInfo"]+"?currentPage="+i+"&onePageCount=10",
+                        data: newData,
+                        contentType: "application/json",
+                        dataType: "json",
+                        success:function(data){
+                             unloading();
+                             $("[list-node]").remove();
+                             $(".gdm-table-data").setPageData(data);
+                             binFun()
+                        }
+                    });
+                });
+                 $("[list-node]").remove();
                 $(".gdm-table-data").setPageData(data);
-                $(".gdm-btn-del").unbind().bind("click", function () {
-                    if (confirm("是否确认删除?")) {
-                        loading();
-                        var productId = $(this).parent().parent().children().first().attr("productId");
-                        $.get(plumeApi["delProductInfo"] + "/" + productId, {}, function (data) {
-                            unloading();
-                            if (data.ok) {
-                                $("[list-node]").remove();
-                                getTableData();
-                                $('.pop').loadTemp("popTips", "nochangeurl", function () {
-                                    $(".pop").find(".popup-title").html("信息提示");
-                                    $(".pop").find(".popup-icon").html('<i class="success"></i>');
-                                    $(".pop").find(".popup-info").html("删除成功");
-                                });
-                            } else {
-                                $('.pop').loadTemp("popTips", "nochangeurl", function () {
-                                    $(".pop").find(".popup-title").html("信息提示");
-                                    $(".pop").find(".popup-icon").html('<i class="warning"></i>');
-                                    $(".pop").find(".popup-info").html("删除失败");
-                                });
-                            }
-                        });
-                    }
-                });
+                binFun()
 
-                $(".gdm-btn-edit").unbind().bind("click", function () {
-                    var productId = $(this).parent().parent().children().first().attr("productId");
-                    session.goods_edit_productId = productId;
-                    derict(this, "editMyGoods", "nochangeurl");
-                });
-                $('.gdm-btn-open').each(function () {
-                    if ($(this).html() == 1) {
-                        $(this).html('禁用');
-                    } else {
-                        $(this).html('启用');
-                    }
-                });
-                $('.gdm-btn-copy').unbind().bind("click", function () {
-                    var productId = $(this).parent().parent().children().first().attr("productId");
-                    session.goods_edit_productId = productId;
-                    derict(this, "copyMyGoods", "nochangeurl");
-                })
-
+              
             }
         });
     }
@@ -164,7 +138,53 @@ $(function () {
     })
 
    
+//按钮绑定方法
+    function binFun(){
+        $(".gdm-btn-del").unbind().bind("click", function () {
+                    if (confirm("是否确认删除?")) {
+                        loading();
+                        var productId = $(this).parent().parent().children().first().attr("productId");
+                        $.get(plumeApi["delProductInfo"] + "/" + productId, {}, function (data) {
+                            unloading();
+                            if (data.ok) {
+                                $("[list-node]").remove();
+                                getTableData();
+                                $('.pop').loadTemp("popTips", "nochangeurl", function () {
+                                    $(".pop").find(".popup-title").html("信息提示");
+                                    $(".pop").find(".popup-icon").html('<i class="success"></i>');
+                                    $(".pop").find(".popup-info").html("删除成功");
+                                });
+                            } else {
+                                $('.pop').loadTemp("popTips", "nochangeurl", function () {
+                                    $(".pop").find(".popup-title").html("信息提示");
+                                    $(".pop").find(".popup-icon").html('<i class="warning"></i>');
+                                    $(".pop").find(".popup-info").html("删除失败");
+                                });
+                            }
+                        });
+                    }
+                });
 
+                  $(".gdm-btn-edit").unbind().bind("click", function () {
+                    var productId = $(this).parent().parent().children().first().attr("productId");
+                    session.goods_edit_productId = productId;
+                    derict(this, "editMyGoods", "nochangeurl");
+                });
+                $('.gdm-btn-open').each(function () {
+                    if ($(this).html() == 1) {
+                        $(this).html('禁用');
+                    } else {
+                        $(this).html('启用');
+                    }
+                });
+                $('.gdm-btn-copy').unbind().bind("click", function () {
+                    var productId = $(this).parent().parent().children().first().attr("productId");
+                    session.goods_edit_productId = productId;
+                    derict(this, "copyMyGoods", "nochangeurl");
+                })
+
+        
+    }
 
 });
 
