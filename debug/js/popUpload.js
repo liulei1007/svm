fc32131f3d895c954e5d84dd80460c8edbd4f0bb
liulei1007 <you@example.com>
2016-05-22 12:39:49 +0800

    liulei

--- debug/js/popUpload.js
+++ debug/js/popUpload.js
@@ -1,29 +1,30 @@
 $(function () {
     plumeLog("进入popUpload模板自定义js-"+plumeTime());
 
-    $(".pu-ok").bind("click", function () {
-        loading();
-        $('#myform').submit();
-    })
-    $('#myform').ajaxForm(function (data) {
-        unloading();
-        if(data.ok){
-            $("#filepath").val(data.data);
-            var temp='<li class="goodsPic">';
-            temp+='<img class="cmg-goodsimgs" src="'+"http://10.11.25.215"+$("#filepath").val()+'">';
-            temp+='<div class="upload-btn upload-btn-left">';
-            temp+='<div class="arrow-left"></div>';
-            temp+='</div>';
-            temp+='<div class="upload-btn upload-btn-right">';
-            temp+='<div class="arrow-right"></div>';
-            temp+='</div>';
-            temp+='<div class="upload-btn upload-btn-delect">';
-            temp+='<div class="arrow-close"></div>';
-            temp+='</div>';
-            temp+='</li>';
-            $(".goodsPic-upload").append(temp);
-        }else{
-            alert(data.resDescription);
-        }
-    });
+    //$(".pu-ok").bind("click", function () {
+    //    loading();
+    //    $('#myform').submit();
+    //})
+    //$('#myform').ajaxForm(function (data) {
+    //    unloading();
+    //    if(data.ok){
+    //        $("#filepath").val(data.data);
+    //        var temp='<li class="goodsPic">';
+    //        temp+='<img class="cmg-goodsimgs" src="'+"http://img2.hxmklmall.cn"+$("#filepath").val()+'">';
+    //        temp+='<div class="upload-btn upload-btn-left">';
+    //        temp+='<div class="arrow-left"></div>';
+    //        temp+='</div>';
+    //        temp+='<div class="upload-btn upload-btn-right">';
+    //        temp+='<div class="arrow-right"></div>';
+    //        temp+='</div>';
+    //        temp+='<div class="upload-btn upload-btn-delect">';
+    //        temp+='<div class="arrow-close"></div>';
+    //        temp+='</div>';
+    //        temp+='</li>';
+    //        $(".goodsPic-upload").append(temp);
+    //        closeUploadPop();
+    //    }else{
+    //        alert(data.resDescription);
+    //    }
+    //});
 })
--- debug/js/showMyGoods.js
+++ debug/js/showMyGoods.js
@@ -9,9 +9,30 @@ $(function () {
             contentType: "application/json",
             dataType: "json",
             success: function (data) {
-                //$(".cmg-brand").setPageData(data);
-                console.log(data);
+
                 unloading();
+                var d = data.data
+                $(".smg-basicInfo,.smg-base-attr").setPageData(d);
+                console.log(d.productInfoAttrUptORMs);
+                for (var i = 0; i < d.productInfoAttrUptORMs.length; i++) {
+                    var p = d.productInfoAttrUptORMs[i];
+                    var temp = '<div class="form-group required smg-base-attr">';
+                    temp += '<label class="col-sm-2 control-label">' + p.productAttribute.attrNameFront + '</label>';
+                    temp += '<div class="col-sm-2">';
+                    temp += '<p class="col-sm-4 form-control-static">' + p.attrValueId + '</p>';
+                    temp += '</div>';
+                    temp += '</div>';
+                    $(".goodsAttr-content").append(temp);
+                }
+                for (var j = 0; j < d.productGoodsUpts.length; j++) {
+                    var p = d.productGoodsUpts[j];
+                    var temp = '<tr class="cmg-goodstr">';
+                    temp += '<td>'+ p.color+'</td>';
+                    temp += '<td>'+ p.standard+'</td>';
+                    temp += '<td>'+ p.salePrice+'</td>';
+                    temp += '</tr>';
+                    $(".standardtbody").append(temp);
+                }
                 //$("#brandId").bind("change", function () {
                 //    loading();
                 //    var brandId = $(this).val();
@@ -24,6 +45,7 @@ $(function () {
             }
         });
     }
+
     getProductInfoUpt();
 
 
