51ab10268bf8a5663a0ef1dc93c9fb942ea3f71f
liulei1007 <you@example.com>
2016-05-31 16:51:42 +0800

    liulei

--- debug/js/changepwd.js
+++ debug/js/changepwd.js
@@ -1,47 +1,50 @@
-$(function(){
-	plumeLog("进入changepwd模板自定义js-"+plumeTime());
-	$("#cpdnext").bind("click",function(){
-		$(".reg-msg1").hide();
-		var verifycode = $('.verifycode').val();
-		if (verifycode == '') {
-			alert("短信验证码发送成功");
-			return;
-		};
+$(function () {
+    plumeLog("进入changepwd模板自定义js-" + plumeTime());
+    $("#cpdnext").bind("click", function () {
+        $(".reg-msg1").hide();
+        var verifycode = $('.verifycode').val();
+        if (verifycode == '') {
+            alert("短信验证码发送成功");
+            return;
+        }
+        ;
 
-		$(".cpdstep1").hide();
-		$(".cpdstep2").fadeIn();
-	});
-	$("#cpdsub").bind("click",function(){
-		$(".reg-msg2").hide();
-		var password = $('.password').val();
-		var repassword = $('.repassword').val();
-		if(password == '') {
-			alert("请输入新密码");
-			return;
-		}
+        $(".cpdstep1").hide();
+        $(".cpdstep2").fadeIn();
+    });
+    $("#cpdsub").bind("click", function () {
+        $(".reg-msg2").hide();
+        var password = $('.password').val();
+        var repassword = $('.repassword').val();
+        if (password == '') {
+            alert("请输入新密码");
+            return;
+        }
 
-		if (repassword == '') {
-			alert("请输入确认密码");
-			return;
-		};
+        if (repassword == '') {
+            alert("请输入确认密码");
+            return;
+        }
+        ;
 
-		if (password != repassword) {
-			alert("秘密不一致");
-			return;
-		};
+        if (password != repassword) {
+            alert("密码不一致");
+            return;
+        }
+        ;
 
-		resetPassword();
+        resetPassword();
 
-	//	$(".cpdstep2").hide();
-	//	$(".cpdstep1").fadeIn();
-	});
+        //	$(".cpdstep2").hide();
+        //	$(".cpdstep1").fadeIn();
+    });
 
-	$("#cpdsendcode").bind("click", function() {
-		if(!sendMsgCount){
-			return;
-		}
-		var mobile = $(".mobile").val();
-		$(".reg-msg1").hide();
+    $("#cpdsendcode").bind("click", function () {
+        if (!sendMsgCount) {
+            return;
+        }
+        var mobile = $(".mobile").val();
+        $(".reg-msg1").hide();
         if (isMobile(mobile)) {
             loading();
             $.get(plumeApi["sendMsg"] + "/" + mobile + "/10003", {}, function (data) {
@@ -50,65 +53,67 @@ $(function(){
                     alert("短信验证码发送成功");
                     settime(10);
                 } else {
-                	alert("短信验证码发送异常");
+                    alert("短信验证码发送异常");
                 }
             });
         } else {
             alert("手机号输入错误");
         }
-	})
+    })
 })
 
 function isMobile(n) {
     return /^1\d{10}$/.test(n) && n != 11111111111;
 }
- var sendMsgCount=true;
-function settime(countdown) { 
-	if (countdown == 0) { 
-		$(".timeshow").html("获取验证码");
-		sendMsgCount = true;
-		return;
-	} else { 
-		$(".timeshow").html(countdown+"s后重新发送");
-		sendMsgCount = false;
-		countdown--;
-		setTimeout(function() {
-			settime(countdown)
-		},1000);
-	} 
+var sendMsgCount = true;
+function settime(countdown) {
+    if (countdown == 0) {
+        $(".timeshow").html("获取验证码");
+        sendMsgCount = true;
+        return;
+    } else {
+        $(".timeshow").html(countdown + "s后重新发送");
+        sendMsgCount = false;
+        countdown--;
+        setTimeout(function () {
+            settime(countdown)
+        }, 1000);
+    }
 
 }
 
 function resetPassword() {
-	var mobile = $(".mobile").val();
-	var verifycode = $('.verifycode').val();
-	var password = $('.password').val();
-	var repassword = $('.repassword').val();
+    var mobile = $(".mobile").val();
+    var verifycode = $('.verifycode').val();
+    var password = $('.password').val();
+    var repassword = $('.repassword').val();
 
-	$(".reg-msg2").hide();
+    $(".reg-msg2").hide();
 
-	loading();
-	$.ajax({
-		url: plumeApi["resetPassword"],
-		type: "POST",
-		data: JSON.stringify({
-				"mobilePhone":mobile,
-				"password":password,
-				"rePassword":repassword,
-				"verifyCode":verifycode
-			}),
-		dataType: "json",
-		contentType: "application/json; charset=utf-8",
-		success: function(data) {
-			unloading();
+    loading();
+    $.ajax({
+        url: plumeApi["resetPassword"],
+        type: "POST",
+        data: JSON.stringify({
+            "mobilePhone": mobile,
+            "password": password,
+            "rePassword": repassword,
+            "verifyCode": verifycode
+        }),
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            unloading();
             if (data.ok) {
-            	alert("密码重置成功");
+                alert("密码重置成功");
                 window.location.href = "login";
             } else {
-            	alert("密码重置失败");
+                alert("密码重置失败");
             }
-		},
-		error:function(error) {console.log(error);}
-	});
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
 
 }
