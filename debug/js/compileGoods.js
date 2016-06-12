cfbaf6376005cd4a8492c8ae9e8af1e3f1f5f859
alex <alexguoliang@163.com>
2016-06-12 14:13:12 +0800

    123132

--- debug/js/compileGoods.js
+++ debug/js/compileGoods.js
@@ -1,13 +1,106 @@
 $(function(){
 	  $(".alert-danger").hide();
 	plumeLog("进入compileGoods模板自定义js-"+plumeTime());
-	getGoodsInfo()
+	getGoodsInfo();
 	$(".btn-compileGoods").on('click','.btn-back',function() {
 		derict(this, "groundGoods", "nochangeurl");
-	})
+	});
 
 	$('.btn-compileGoods').on('click','.btn-next',function() {
-		loading()
-		editProductShopGoods()
-	})
-})
+		loading();
+		editProductShopGoods();
+	});
+
+
+	//获取商品信息
+function getGoodsInfo() {
+    try {
+        loading();
+        $.ajax({
+            url: plumeApi["getProductShopGoods"] + session.goods_psgId,
+            type: "GET",
+            contentType: "application/json;charset=UTF-8",
+            success: function (data) {
+                if(data.ok){
+                    unloading();
+                    if(data.data.productGoodsORM.productInfoORM.baseCategoryId==1){
+                        $(".material").show();
+                        $(".material_temp").hide();
+                     }else{
+                        $(".material").hide();
+                        $(".material_temp").show();
+                    }
+                    $('.body-typein').setPageData(data.data);
+                    var formsList = "";
+                    $(data.data.productGoodsORM.productInfoORM.productInfoAttrORMs).each(function (i, good) {
+                        formsList += '<div class="form-group"><label class="col-sm-3 control-label">' + good.productAttribute.attrNameBack + '：</label><p class="col-sm-6 form-control-static">' + good.attrValue + '</p></div>'
+                    });
+                    $(".forms-block").append(formsList);
+                    $(".weight-info").html($(".weight-info").html()+"KG")
+                    $(".priceType-info").text(setListSystemCode(JSON.parse(session.price_tpye),$(".priceType-info").text()));
+                    $(".level-info").text(setListSystemCode(JSON.parse(session.product_lv),$(".level-info").text()));
+                     if(data.data.saleStatus==1){
+                         $(".taking-size input").eq(0).attr('checked','checked');
+                     }else{
+                        $(".taking-size input").eq(1).attr('checked','checked');
+                     }
+                     if(data.data.priceType==1){
+                        $("#priceType option").eq(0).attr('selected','selected');
+                     }else{
+                        $("#priceType option").eq(1).attr('selected','selected');
+                     }
+                }else{
+                     unloading();
+                      $('.pop').loadTemp("popTips", "nochangeurl", function () {
+                        $(".pop").find(".popup-title").html(data.resDescription);
+                        $(".pop").find(".popup-icon").html('<i class="warning"></i>');
+                        $(".pop").find(".popup-info").html("");
+                        derict(this, "groundGoods", "nochangeurl");
+                    });
+                }
+            }
+        })
+    } catch (e) {
+        window.location.href = "/";
+    }
+}
+
+
+//编辑店铺商品
+function editProductShopGoods() {
+    var price = $("#price").val();
+    var priceType = $("#priceType").val();
+    var inventory = $("#inventory").val();
+    var saleStatus = $("input[name='ground']:checked").val()
+    loading();
+    $.ajax({
+        url: plumeApi["editProductShopGoods"],
+        type: "POST",
+        contentType: "application/json;charset=UTF-8",
+        data: JSON.stringify(
+            {
+                "psgId": session.goods_psgId,
+                "salePrice": price,     
+                "priceType": priceType,
+                "inventory": inventory,
+                "saleStatus": saleStatus
+            }
+        ),
+        success: function (data) {
+            if (data.ok) {
+                unloading();
+                popTips("商品编辑成功", "success");
+                derict(this, "groundGoods", "nochangeurl");
+            } else {
+                unloading();
+                 $('.pop').loadTemp("popTips", "nochangeurl", function () {
+                    $(".pop").find(".popup-title").html("商品编辑失败");
+                    $(".pop").find(".popup-icon").html('<i class="warning"></i>');
+                    $(".pop").find(".popup-info").html(data.resDescription);
+                  });
+            }
+        }
+    });
+}
+
+});
--- debug/js/pagination.js
+++ debug/js/pagination.js
@@ -1,120 +1,137 @@
-// nowPage: 当前页数, totalPage: 总共页数
-var nowPage = 1, totalPage = 20;
+function newPage(totalPage, fun) {
+    var Tf = false
+    var nowPage = 1;
+    loadPagination(nowPage, totalPage);
 
-$(function() {
-	loadPagination(nowPage, totalPage);
+    // 如果总页数大于5, 还需显示跳转表单
+    if (totalPage > 10) {
+        var turnHtml = '';
+        turnHtml += '<div class="turn">';
+        turnHtml += '<span>跳转到</span>';
+        turnHtml += '<input type="text" id="turnTo" class="form-control" />';
+        turnHtml += '<div class="btn btn-default btn-go">GO</div>';
+        turnHtml += '</div>';
+        $(".nav-pagination").prepend(turnHtml);
+    }
 
-	// 如果总页数大于5, 还需显示跳转表单
-	if (totalPage > 10) {
-		var turnHtml = '';
-		turnHtml += '<div class="turn">';
-		turnHtml += '<span>跳转到</span>';
-		turnHtml += '<input type="text" id="turnTo" class="form-control" />';
-		turnHtml += '<div class="btn btn-default btn-go">GO</div>';
-		turnHtml += '</div>';
-		$(".nav-pagination").prepend(turnHtml);
-	}
+    // 绑定分页点击事件
+    $(".nav-pagination").on("click", ".num", function () {
+        // 防止点击当前页
+        if ($(this).hasClass("active")) {
+            return;
+        }
 
-	// 绑定分页点击事件
-	$(".nav-pagination").on("click", ".num", function() {
-		// 防止点击当前页
-		if ($(this).hasClass("active")) { return; }
+        nowPage = parseInt($(this).attr("data-page"));
+        loadPagination(nowPage, totalPage);
+    }).on("click", ".first", function () {
+        // 防止当前已是最前页
+        if ($(this).hasClass("disabled")) {
+            return;
+            
+        }
 
-		nowPage = parseInt($(this).attr("data-page"));
-		loadPagination(nowPage, totalPage);
-		alert(nowPage);
-	}).on("click", ".first", function() {
-		// 防止当前已是最前页
-		if ($(this).hasClass("disabled")) { return; }
+        nowPage = parseInt($(".nav-pagination").find(".num").eq(0).attr("data-page"));
+        loadPagination(nowPage, totalPage);
+    }).on("click", ".last", function () {
+        // 防止当前已是最后页
+        if ($(this).hasClass("disabled")) {
+            return;
+        }
 
-		nowPage = parseInt($(".nav-pagination").find(".num").eq(0).attr("data-page"));
-		loadPagination(nowPage, totalPage);
-	}).on("click", ".last", function() {
-		// 防止当前已是最后页
-		if ($(this).hasClass("disabled")) { return; }
+        nowPage = parseInt($(".nav-pagination").find(".num").last().attr("data-page"));
+        loadPagination(nowPage, totalPage);
+    });
+    // 绑定跳转页面
+    $(".nav-pagination").on("click", ".btn-go", function () {
+        // 确保输入的数字
+        if (!isNaN(parseInt($("#turnTo").val()))) {
+            nowPage = parseInt($("#turnTo").val());
+            loadPagination(nowPage, totalPage);
+        }
+    });
 
-		nowPage = parseInt($(".nav-pagination").find(".num").last().attr("data-page"));
-		loadPagination(nowPage, totalPage);
-	});
-	// 绑定跳转页面
-	$(".nav-pagination").on("click", ".btn-go", function() {
-		// 确保输入的数字
-		if (!isNaN(parseInt($("#turnTo").val()))) {
-			nowPage = parseInt($("#turnTo").val());
-			loadPagination(nowPage, totalPage);
-		}
-	});
-});
 
-function loadPagination(nowPage, totalPage) {
-	var paginationHtml = '';
-	// 回到最前页按钮
-	if (nowPage == 1) { paginationHtml += '<li class="first disabled"><span>&laquo;</span></li>'; }
-	else paginationHtml += '<li class="first"><span>&laquo;</span></li>';
+    function loadPagination(nowPage, totalPage) {
 
-	// 如果总页数小于或者等于10
-	if (totalPage <= 10) {
-		for (var i = 0; i < totalPage; i++) {
-			if (i == nowPage - 1) { paginationHtml += '<li class="num active" data-page="' + (i + 1) + '"><span>' + (i + 1) + '</span></li>'; }
-			else paginationHtml += '<li class="num" data-page="' + (i + 1) + '"><span>' + (i + 1) + '</span></li>';
-		}
-	}
-	// 如果总页数大于10, 需显示"..."
-	else {
-		// 如果当前页 < 6, 左侧显示7个页码, 右侧显示2个页码
-		if (nowPage < 6) {
-			// 左侧7个页码
-			for (var i = 0; i < 7; i++) {
-				if (i == nowPage - 1) { paginationHtml += '<li class="num active" data-page="' + (i + 1) + '"><span>' + (i + 1) + '</span></li>'; }
-				else paginationHtml += '<li class="num" data-page="' + (i + 1) + '"><span>' + (i + 1) + '</span></li>';
-			}
-			// 中间"..."
-			paginationHtml += '<li class="more"><span>...</span></li>';
-			// 右侧2个页码
-			paginationHtml += '<li class="num" data-page="' + (totalPage - 1) + '"><span>' + (totalPage - 1) + '</span></li>';
-			paginationHtml += '<li class="num" data-page="' + totalPage + '"><span>' + totalPage + '</span></li>';
-		}
-		// 如果总页数 - 当前页 < 5, 左侧显示2个页码, 右侧显示7个页码
-		else if (totalPage - nowPage < 5) {
-			// 左侧2个页码
-			paginationHtml += '<li class="num" data-page="1"><span>1</span></li>';
-			paginationHtml += '<li class="num" data-page="2"><span>2</span></li>';
-			// 中间"..."
-			paginationHtml += '<li class="more"><span>...</span></li>';
-			// 右侧7个页码
-			for (var i = totalPage - 7; i < totalPage; i++) {
-				if (i == nowPage - 1) { paginationHtml += '<li class="num active" data-page="' + (i + 1) + '"><span>' + (i + 1) + '</span></li>'; }
-				else paginationHtml += '<li class="num" data-page="' + (i + 1) + '"><span>' + (i + 1) + '</span></li>';
-			}
-		}
-		// 否则, 左侧显示1个页码, 中间显示5个页码, 右侧显示2个页码
-		else {
-			// 左侧2个页码
-			paginationHtml += '<li class="num" data-page="1"><span>1</span></li>';
-			paginationHtml += '<li class="num" data-page="2"><span>2</span></li>';
-			// 中间"..."
-			paginationHtml += '<li class="more"><span>...</span></li>';
-			// 中间5个页码
-			for (var i = (nowPage - 3); i < (nowPage + 2); i++) {
-				if (i == nowPage - 1) { paginationHtml += '<li class="num active" data-page="' + (i + 1) + '"><span>' + (i + 1) + '</span></li>'; }
-				else paginationHtml += '<li class="num" data-page="' + (i + 1) + '"><span>' + (i + 1) + '</span></li>';
-			}
-			// 中间"..."
-			paginationHtml += '<li class="more"><span>...</span></li>';
-			// 右侧2个页码
-			paginationHtml += '<li class="num" data-page="' + (totalPage - 1) + '"><span>' + (totalPage - 1) + '</span></li>';
-			paginationHtml += '<li class="num" data-page="' + totalPage + '"><span>' + totalPage + '</span></li>';
-		}
-	}
+        var paginationHtml = '';
+        // 回到最前页按钮
+        if (nowPage == 1&&totalPage>0) {
+            paginationHtml += '<li class="first disabled"><span>&laquo;</span></li>';
+        }
+        else if(totalPage>0){ paginationHtml += '<li class="first"><span>&laquo;</span></li>';}
 
-	// 跳到最后页按钮
-	if (nowPage == totalPage) { paginationHtml += '<li class="last disabled"><span>&raquo;</span></li>'; }
-	else paginationHtml += '<li class="last"><span>&raquo;</span></li>';
-	$(".pagination").html(paginationHtml);
-	turnToPage(nowPage);
-}
+        // 如果总页数小于或者等于10
+        if (totalPage <= 10) {
+            for (var i = 0; i < totalPage; i++) {
+                if (i == nowPage - 1) {
+                    paginationHtml += '<li class="num active" data-page="' + (i + 1) + '"><span>' + (i + 1) + '</span></li>';
+                }
+                else paginationHtml += '<li class="num" data-page="' + (i + 1) + '"><span>' + (i + 1) + '</span></li>';
+            }
+        }
+        // 如果总页数大于10, 需显示"..."
+        else {
+            // 如果当前页 < 6, 左侧显示7个页码, 右侧显示2个页码
+            if (nowPage < 6) {
+                // 左侧7个页码
+                for (var i = 0; i < 7; i++) {
+                    if (i == nowPage - 1) {
+                        paginationHtml += '<li class="num active" data-page="' + (i + 1) + '"><span>' + (i + 1) + '</span></li>';
+                    }
+                    else paginationHtml += '<li class="num" data-page="' + (i + 1) + '"><span>' + (i + 1) + '</span></li>';
+                }
+                // 中间"..."
+                paginationHtml += '<li class="more"><span>...</span></li>';
+                // 右侧2个页码
+                paginationHtml += '<li class="num" data-page="' + (totalPage - 1) + '"><span>' + (totalPage - 1) + '</span></li>';
+                paginationHtml += '<li class="num" data-page="' + totalPage + '"><span>' + totalPage + '</span></li>';
+            }
+            // 如果总页数 - 当前页 < 5, 左侧显示2个页码, 右侧显示7个页码
+            else if (totalPage - nowPage < 5) {
+                // 左侧2个页码
+                paginationHtml += '<li class="num" data-page="1"><span>1</span></li>';
+                paginationHtml += '<li class="num" data-page="2"><span>2</span></li>';
+                // 中间"..."
+                paginationHtml += '<li class="more"><span>...</span></li>';
+                // 右侧7个页码
+                for (var i = totalPage - 7; i < totalPage; i++) {
+                    if (i == nowPage - 1) {
+                        paginationHtml += '<li class="num active" data-page="' + (i + 1) + '"><span>' + (i + 1) + '</span></li>';
+                    }
+                    else paginationHtml += '<li class="num" data-page="' + (i + 1) + '"><span>' + (i + 1) + '</span></li>';
+                }
+            }
+            // 否则, 左侧显示1个页码, 中间显示5个页码, 右侧显示2个页码
+            else {
+                // 左侧2个页码
+                paginationHtml += '<li class="num" data-page="1"><span>1</span></li>';
+                paginationHtml += '<li class="num" data-page="2"><span>2</span></li>';
+                // 中间"..."
+                paginationHtml += '<li class="more"><span>...</span></li>';
+                // 中间5个页码
+                for (var i = (nowPage - 3); i < (nowPage + 2); i++) {
+                    if (i == nowPage - 1) {
+                        paginationHtml += '<li class="num active" data-page="' + (i + 1) + '"><span>' + (i + 1) + '</span></li>';
+                    }
+                    else paginationHtml += '<li class="num" data-page="' + (i + 1) + '"><span>' + (i + 1) + '</span></li>';
+                }
+                // 中间"..."
+                paginationHtml += '<li class="more"><span>...</span></li>';
+                // 右侧2个页码
+                paginationHtml += '<li class="num" data-page="' + (totalPage - 1) + '"><span>' + (totalPage - 1) + '</span></li>';
+                paginationHtml += '<li class="num" data-page="' + totalPage + '"><span>' + totalPage + '</span></li>';
+            }
+        }
 
-// 跳转页面的操作
-function turnToPage(nowPage) {
-	console.log(nowPage);
+        // 跳到最后页按钮
+        if (nowPage == totalPage) {
+            paginationHtml += '<li class="last disabled"><span>&raquo;</span></li>';
+        }
+        else if(totalPage>0){paginationHtml += '<li class="last"><span>&raquo;</span></li>';}
+        $(".pagination").html(paginationHtml);
+        if (fun) {
+            if (Tf) {fun(nowPage);}
+            Tf = true;
+        }
+    }
 }
