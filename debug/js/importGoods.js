211a3d6b53c1c791fcd273df4a2822852c4bfa62
alex <alexguoliang@163.com>
2016-06-28 11:05:51 +0800

    1321312

--- debug/js/importGoods.js
+++ debug/js/importGoods.js
@@ -1,5 +1,4 @@
 $(function () {
-    var timer = null;
     session.goods_baseCategoryId = "";
     session.goods_subCategoryId = "";
     session.goods_categoryId = "";
@@ -20,6 +19,7 @@ $(function () {
            $('#myForm').ajaxForm(function (data) {
                unloading();
                if (data.ok) {
+                $(".ex-ok").bind("click", importUpLoad);
                    alert("上传成功");
                    $('.pop').hide();
                } else {
@@ -31,23 +31,7 @@ $(function () {
                $(".pop").hide();
            });
 
-           $(".ex-ok").bind("click", function () {
-              clearTimeout(timer);
-               var filePath  = ($('#fileSuffix').html());
-               var fileSuffix = filePath.substring(filePath.lastIndexOf(".")+1);
-               if(fileSuffix!="xlsx"&&fileSuffix!=""&&fileSuffix!="xls"){
-                  alert("上传格式不正确");
-               }else if(!session.goods_baseCategoryId){
-                alert("请选择分类");
-               }
-               if (($("#file").val()&&(fileSuffix=="xlsx"||fileSuffix=="xls")&&session.goods_baseCategoryId)) {
-                   document.myForm.action = plumeApi["uploadEx"] + session.goods_baseCategoryId + "/" + session.goods_subCategoryId + "/" + session.goods_categoryId
-
-                   timer = setTimeout(function(){
-                      $('#myForm').submit();
-                   } ,2000)           
-               }
-           });
+           $(".ex-ok").bind("click", importUpLoad);
 
            $(".btn-loadModule").bind("click", function () {
                if (session.goods_baseCategoryId) {
@@ -58,6 +42,20 @@ $(function () {
                }
            });
 
+          function importUpLoad() {
+              $(".ex-ok").unbind();
+               var filePath  = ($('#fileSuffix').html());
+               var fileSuffix = filePath.substring(filePath.lastIndexOf(".")+1);
+               if(fileSuffix!="xlsx"&&fileSuffix!=""&&fileSuffix!="xls"){
+                  alert("上传格式不正确");
+               }else if(!session.goods_baseCategoryId){
+                alert("请选择分类");
+               }
+               if (($("#file").val()&&(fileSuffix=="xlsx"||fileSuffix=="xls")&&session.goods_baseCategoryId)) {
+                   document.myForm.action = plumeApi["uploadEx"] + session.goods_baseCategoryId + "/" + session.goods_subCategoryId + "/" + session.goods_categoryId
+                      $('#myForm').submit();         
+               }
+          }
    // })
   
 })
