de8e51fc0fafcd672188849c9679d42bbd3448d0
alex <alexguoliang@163.com>
2016-06-30 16:17:09 +0800

    31323

--- debug/js/importGoods.js
+++ debug/js/importGoods.js
@@ -1,81 +1,113 @@
 $(function () {
-    session.goods_baseCategoryId = "";
-    session.goods_subCategoryId = "";
-    session.goods_categoryId = "";
 
-    $(".close").bind("click", function() {
-        $(this).parents(".alert").hide();
-    });
-    $(".btn-chooseSort").bind("click", function() {
-    	$(".popSort").loadTemp("popUserType", "nochangeurl", function () {
-            
-        });
-    });
-    $(".btn-cancel").bind("click",function() {
-    	$(".pop").hide();
-    });
+    var importGoods={
 
-       // $(".btn-import-data").bind("click", function () {
-           $('#myForm').ajaxForm(function (data) {
-               if (data.ok) {
-                unloading();
-                $(".loadFile").val("");
-                $('#fileSuffix').html("");
-                showPopTips('上传成功',"warning","");
+        //初始参数
+      init: function() {
+        session.goods_baseCategoryId = "";
+        session.goods_subCategoryId = "";
+        session.goods_categoryId = "";
+
+        return this;
+      },
+
+      //事件绑定
+      bindEvent: function() {
+          var $_this = this;
+          $(".close").bind("click", function() {
+            $(this).parents(".alert").hide();
+          });
+          $(".btn-chooseSort").bind("click", function() {
+            $(".popSort").loadTemp("popUserType", "nochangeurl", function () {
+            });
+          });
+          $(".btn-cancel").bind("click",function() {
+            $(".pop").hide();
+          });
+          $('.btn-cancel').bind('click', function () {
+            $(".pop").hide();
+          });
+          $(".ex-ok").bind("click", $_this.submitForm);
+          $(".btn-loadModule").bind("click", function () {
+            if (session.goods_baseCategoryId) {
+              var count = $(".btn-count input").val();
+              $_this.downloadModule();
+            } else {
+              unloading();
+              showPopTips('请选择分类',"warning","");
+            }
+          });
+
+
+          return $_this;
+      },
+
+
+
+
+      //页面ajaxForm请求
+      ajaxForm: function() {
+        $('#myForm').ajaxForm(function (data) {
+          if (data.ok) {
+            unloading();
+            $(".loadFile").val("");
+            $('#fileSuffix').html("");
+            showPopTips('上传成功',"warning","");
                 //$('.pop').hide();
-               } else {
-                  unloading();
-                  alert(data.resDescription || data.data);
-                  $('.pop').hide();
-               }
-           });
-           $('.btn-cancel').bind('click', function () {
-               $(".pop").hide();
-           });
-
-           $(".ex-ok").bind("click", importUpLoad);
-
-           $(".btn-loadModule").bind("click", function () {
-               if (session.goods_baseCategoryId) {
-                   var count = $(".btn-count input").val();
-                   window.location = plumeApi["downloadEx"] + session.goods_baseCategoryId + "/" + session.goods_subCategoryId + "/" + session.goods_categoryId + "?count=1000"
-               } else {
-                  unloading();
-                  showPopTips('请选择分类',"warning","");
-
-               }
-           });
-
-          function importUpLoad() {
-
-               var filePath  = ($('#fileSuffix').html());
-
-               var fileSuffix = filePath.substring(filePath.lastIndexOf(".")+1);
-
-               if(fileSuffix!="xlsx"&&fileSuffix!=""&&fileSuffix!="xls"){
-
-                showPopTips('上传格式不正确',"warning","");
-
-               }else if(!session.goods_baseCategoryId){
-
-                showPopTips('请选择分类',"warning","");
-
-               }else if(fileSuffix==""){
-
-                showPopTips('未选择上传文件',"warning","");  
-              
-               }
-
-               if (($("#file").val()&&(fileSuffix=="xlsx"||fileSuffix=="xls")&&session.goods_baseCategoryId)) {
-
-                  loading();
-
-                  document.myForm.action = plumeApi["uploadEx"] + session.goods_baseCategoryId + "/" + session.goods_subCategoryId + "/" + session.goods_categoryId
-
-                  $('#myForm').submit();         
-                
-               }
+          } else {
+            unloading();
+            alert(data.resDescription || data.data);
+            $('.pop').hide();
           }
-   // })
-  
+        });
+      },
+
+
+      //ajax提交
+      submitForm: function() {
+        $_this = this;
+        var filePath  = ($('#fileSuffix').html());
+
+        var fileSuffix = filePath.substring(filePath.lastIndexOf(".")+1);
+
+        if(fileSuffix!="xlsx"&&fileSuffix!=""&&fileSuffix!="xls"){
+
+          showPopTips('上传格式不正确',"warning","");
+
+        }else if(!session.goods_baseCategoryId){
+
+        showPopTips('请选择分类',"warning","");
+
+        }else if(fileSuffix==""){
+
+          showPopTips('未选择上传文件',"warning","");  
+              
+        }
+
+        if (($("#file").val()&&(fileSuffix=="xlsx"||fileSuffix=="xls")&&session.goods_baseCategoryId)) {
+
+           loading();
+
+           document.myForm.action = plumeApi["uploadEx"] + session.goods_baseCategoryId + "/" + session.goods_subCategoryId + "/" + session.goods_categoryId
+
+           $('#myForm').submit();         
+                
+        }
+      },
+
+
+      //下载模板
+      downloadModule: function() {
+        window.location = plumeApi["downloadEx"] + session.goods_baseCategoryId + "/" + session.goods_subCategoryId + "/" + session.goods_categoryId + "?count=1000"
+      },
+
+
+      //控制器
+      importGoodsController: function() {
+        this.init().bindEvent().ajaxForm();
+      }
+    }
+
+    importGoods.importGoodsController();
+   
 })
--- debug/js/importSelfGoods.js
+++ debug/js/importSelfGoods.js
@@ -1,48 +1,76 @@
 $(function () {
-    $(".close").bind("click", function() {
+  var importSelfGoods = {
+    bindEvent: function() {
+      $_this = this;
+      $(".close").bind("click", function() {
         $(this).parents(".alert").hide();
-    });
-    $(".btn-chooseSort").bind("click", function() {
-    	$(".popSort").loadTemp("popUserType", "nochangeurl", function () {
+      });
+      $(".btn-chooseSort").bind("click", function() {
+        $(".popSort").loadTemp("popUserType", "nochangeurl", function () {});
+      });
+      $(".btn-cancel").bind("click",function() {
+        $(".pop").hide();
+      });
+      $(".btn-loadModule").bind("click", function() {
+        window.location = plumeApi["exportProductStashTemplate"] + "?count=1000"
+      });
+      $(".ex-ok").bind("click", $_this.submitForm);
+
+      return $_this;
+    },
+
+    //页面ajaxForm请求
+    ajaxForm: function() {
+      $('#myForm').ajaxForm(function (data) {
+        if (data.ok) {
+          unloading();
+          $(".loadFile").val("");
+          $('#fileSuffix').html("");
+          showPopTips('上传成功',"warning","");
+        } else {
+          unloading();
+          alert(data.resDescription || data.data);
+          $('.pop').hide();
+        }
+      });
+    },
+
+
+
+    //ajax提交
+    submitForm: function() {
+      $_this = this;
+      var filePath  = ($('#fileSuffix').html());
+
+      var fileSuffix = filePath.substring(filePath.lastIndexOf(".")+1);
+
+      if(fileSuffix!="xlsx"&&fileSuffix!=""&&fileSuffix!="xls"){
+
+        showPopTips('上传格式不正确',"warning","");
+
+      }else if(fileSuffix==""){
+
+        showPopTips('未选择上传文件',"warning","");  
             
-        });
-    });
-    $(".btn-cancel").bind("click",function() {
-    	$(".pop").hide();
-    });
+      }
 
-       // $(".btn-import-data").bind("click", function () {
-           $('#myForm').ajaxForm(function (data) {
-               unloading();
-               if (data.ok) {
-                   alert("上传成功");
-                   $('.pop').hide();
-               } else {
-                   alert(data.resDescription || data.data);
-                   $('.pop').hide();
-               }
-           });
-           $('.btn-cancel').bind('click', function () {
-               $(".pop").hide();
-           });
+      if (($("#file").val()&&(fileSuffix=="xlsx"||fileSuffix=="xls"))) {
 
+        loading();
 
-           $(".ex-ok").bind("click", function () {
-               var filePath  = ($('#fileSuffix').html());
-               var fileSuffix = filePath.substring(filePath.lastIndexOf(".")+1);
-               if(fileSuffix!="xlsx"&&fileSuffix!=""&&fileSuffix!="xls"){
-                  alert("上传格式不正确");
-               }
-               if (($("#file").val()&&(fileSuffix=="xlsx"||fileSuffix=="xls"))) {
-                   document.myForm.action = plumeApi["importProductStash"]
-                   $('#myForm').submit();  
-               }
-           });
+        document.myForm.action = plumeApi["importProductStash"]
 
-           $(".btn-loadModule").bind("click", function () {
-                  window.location = plumeApi["exportProductStashTemplate"] + "?count=1000"
-           });
+        $('#myForm').submit();         
+              
+      }
+    },
 
-   // })
-  
+    //控制器
+    importSelfGoodsController: function() {
+      this.bindEvent().ajaxForm();
+    }
+  }
+
+   importSelfGoods.importSelfGoodsController();
+
 })
