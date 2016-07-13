9c45cde626583f016bebdd59f9511709e398531c
xiaodousi2014 <646539110@qq.com>
2016-07-13 18:30:58 +0800

    new project

--- wms/js/login.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by lenovo on 2016/7/11.
- */
-(function () {
-    $('body').on('click', '#submit', function () {
-        window.location.href = 'index';
-        return false;
-    });
-})();
